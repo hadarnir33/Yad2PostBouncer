4d6a71948983fe1b24de461e4fa13192d4d0fee8
Hadar Nir <hadarnir33@gmail.com>
2021-01-09 19:41:08 +0200

    Add logger

--- app.py
+++ app.py
@@ -2,9 +2,19 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 import sys
+import logging
 
 TIME_FOR_ACTION_TO_FINISH = 5
 URL = "https://my.yad2.co.il/login.php"
+
+
+def init_logger():
+    logging.basicConfig(
+        filename='logs/app.log',
+        level=logging.INFO,
+        format='%(asctime)s %(message)s',
+        datefmt='%m/%d/%Y %I:%M:%S %p'
+    )
 
 
 def init_driver(url, chrome_driver_path):
@@ -42,6 +52,7 @@ def bounce_post(driver, yad2_item_name, yad2_item_iframe):
     item_bounce_button = driver.find_element_by_xpath(
         "//span[@id='bounceRatingOrderBtn']")
     item_bounce_button.click()
+    wait_for_action_to_finish()
 
 
 def wait_for_action_to_finish():
@@ -49,12 +60,22 @@ def wait_for_action_to_finish():
 
 
 if __name__ == "__main__":
+    init_logger()
+    logging.info('Running app.py')
     chrome_driver_path = sys.argv[1]
     yad2_username = sys.argv[2]
     yad2_password = sys.argv[3]
     yad2_item_name = sys.argv[4]
     yad2_item_iframe = sys.argv[5]
-    driver = init_driver(URL, chrome_driver_path)
-    login(driver, yad2_username, yad2_password)
-    bounce_post(driver, yad2_item_name, yad2_item_iframe)
-    driver.close()
+    logging.info("The item is {}".format(yad2_item_name))
+    try:
+        driver = init_driver(URL, chrome_driver_path)
+        login(driver, yad2_username, yad2_password)
+        bounce_post(driver, yad2_item_name, yad2_item_iframe)
+    except:
+        e = sys.exc_info()[0]
+        logging.error(e)
+        exit(1)
+    finally:
+        driver.close()
+    logging.info("The program finished successfully")
